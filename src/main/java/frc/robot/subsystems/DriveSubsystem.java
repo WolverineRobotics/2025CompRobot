a9de8cff57ec6b5274d9481a04ad6b4b99fce479
muttian001 <154477378+muttian001@users.noreply.github.com>
2025-02-10 15:53:22 -0500

    Began Implementing Autonomous with PathPlanner

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -3,6 +3,14 @@ package frc.robot.subsystems;
 import java.io.File;
 import java.io.IOException;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.commands.PathfindingCommand;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -25,23 +33,20 @@ public class DriveSubsystem extends SubsystemBase {
 
     SwerveDrive swerveDrive;
     StructPublisher<Pose2d> posePublisher;
+    
 
     public DriveSubsystem(File directory, double maximumSpeed) throws IOException {
         swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
         posePublisher = NetworkTableInstance.getDefault().getStructTopic("Robot Pose", Pose2d.struct).publish();
+        setupPathPlanner();
+
+          
     }
-
-    
-
-
-    
-    
  
     public void drive(double translationX, double translationY, double headingX) {
             ChassisSpeeds targetSpeeds = swerveDrive.swerveController.getTargetSpeeds(translationX, translationY, headingX, swerveDrive.getOdometryHeading().getRadians(), swerveDrive.getMaximumChassisVelocity());
             swerveDrive.driveFieldOriented(targetSpeeds);   
-            posePublisher.set(swerveDrive.getPose());
-            
+            posePublisher.set(swerveDrive.getPose());        
         }
     
     public void driveToPoint(Pose2d point) {
@@ -51,8 +56,7 @@ public class DriveSubsystem extends SubsystemBase {
         double netDistance = Math.sqrt((xDistance * xDistance) + (yDistance * yDistance));
         double time = netDistance / DriveSubsystemConstants.kAlignVelocity;
         ChassisSpeeds speeds = new ChassisSpeeds((xDistance / time), (yDistance / time), 0);
-        swerveDrive.driveFieldOriented(speeds);
-       
+        swerveDrive.driveFieldOriented(speeds);   
     }
 
     @Override 
@@ -72,19 +76,79 @@ public class DriveSubsystem extends SubsystemBase {
 
     public void zero() {
         swerveDrive.zeroGyro();
-    }
-
-    private double getDirection(double number) {
-        if (number < 0) {
-            return -1;
-        }
-
-        else {
-            return 1; 
-        }
-    }
-
+    }   
     
+    public void setupPathPlanner()
+    {
+        // Load the RobotConfig from the GUI settings. You should probably
+        // store this in your Constants file
+        RobotConfig config;
+        try
+        {
+        config = RobotConfig.fromGUISettings();
+
+        final boolean enableFeedforward = true;
+        // Configure AutoBuilder last
+        AutoBuilder.configure(
+            swerveDrive::getPose,
+            // Robot pose supplier
+            swerveDrive::resetOdometry,
+            // Method to reset odometry (will be called if your auto has a starting pose)
+            swerveDrive::getRobotVelocity,
+            // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speedsRobotRelative, moduleFeedForwards) -> {
+                if (enableFeedforward)
+                {
+                swerveDrive.drive(
+                    speedsRobotRelative,
+                    swerveDrive.kinematics.toSwerveModuleStates(speedsRobotRelative),
+                    moduleFeedForwards.linearForces()
+                                );
+                } else
+                {
+                swerveDrive.setChassisSpeeds(speedsRobotRelative);
+                }
+            },
+            // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+            new PPHolonomicDriveController(
+                // PPHolonomicController is the built in path following controller for holonomic drive trains
+                new PIDConstants(5.0, 0.0, 0.0),
+                // Translation PID constants
+                new PIDConstants(5.0, 0.0, 0.0)
+                // Rotation PID constants
+            ),
+            config,
+            // The robot configuration
+            () -> {
+                // Boolean supplier that controls when the path will be mirrored for the red alliance
+                // This will flip the path being followed to the red side of the field.
+                // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                var alliance = DriverStation.getAlliance();
+                if (alliance.isPresent())
+                {
+                return alliance.get() == DriverStation.Alliance.Red;
+                }
+                return false;
+            },
+            this
+            // Reference to this subsystem to set requirements
+                            );
+
+        } catch (Exception e)
+        {
+        // Handle exception as needed
+        e.printStackTrace();
+        }
+        //Preload PathPlanner Path finding
+        // IF USING CUSTOM PATHFINDER ADD BEFORE THIS LINE
+        PathfindingCommand.warmupCommand().schedule();
+    }
+
+    public Command getAutoCommand(String pathName) {
+        return new PathPlannerAuto(pathName);
+    }
+
 }
 
 
