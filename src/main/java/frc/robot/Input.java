0bbc9fce38f08f0bb3cee0a0c21bbe5183849196
Ouen <116931732+ozenr@users.noreply.github.com>
2025-02-22 16:52:30 -0500

    Fixed robot orientation

--- src/main/java/frc/robot/Input.java
+++ src/main/java/frc/robot/Input.java
@@ -6,15 +6,15 @@ public class Input {
     public static XboxController driveController = new XboxController(0);
 
     public static double getHorizontal() {
-        return driveController.getLeftX() * -0.1;
+        return driveController.getLeftX() * 0.1;
     }
 
     public static double getRotation() {
-        return driveController.getRightX() * -0.1;
+        return driveController.getRightX() * 0.1;
     }
 
     public static double getVertical() {
-        return driveController.getLeftY() * -0.1;
+        return driveController.getLeftY() * 0.1;
     }
 
     public static Boolean turnRight() {
